9930fb40fa80afa9e4298147fb376b0c70741c2a
pikruse <pikruser@gmail.com>
2025-03-11 08:40:23 -0400

    added scripts dir, training file

---
+++ scripts/train.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from huggingface_hub import login
+from esm.models.esm3 import ESM3
+from esm.sdk.api import ESM3InferenceClient, ESMProtein, GenerationConfig
+
+# -----------------------
+## CONFIG SETTINGS
+# -----------------------
+
+# -----------------------
+## LOAD DATA
+# -----------------------
+
+# -----------------------
+## LOAD MODEL
+# -----------------------
+
+# -----------------------
+## TRAIN MODEL
+# -----------------------
+
+# -----------------------
+## SAVE
+# -----------------------
